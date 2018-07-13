cb4ee33ce2eec41973d62b1367c29ae9fc04e8a2
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-07-13 14:57:59 +0200

    Use UUID as primary keys

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module RncsWorkerApiEntreprise
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+        g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
