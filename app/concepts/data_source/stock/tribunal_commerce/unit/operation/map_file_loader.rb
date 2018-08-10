099ed914116cecde4176d30cdcfcf29cdff96b99
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-08-10 13:02:11 +0200

    Add a task to map stock files to their loader operations

---
+++ app/concepts/data_source/stock/tribunal_commerce/unit/operation/map_file_loader.rb
@@ -0,0 +1,50 @@
+module DataSource
+  module Stock
+    module TribunalCommerce
+      module Unit
+        module Operation
+          class MapFileLoader
+            class MapError < StandardError; end
+
+            class << self
+              def call(ctx, stock_list:, **)
+                begin
+                  stock_list.each do |file_param|
+                    if file_param[:label].nil?
+                      raise MapError, 'Incomming params does not have :label key'
+                    else
+                      file_param[:loader] = fetch_loader(file_param[:label])
+                    end
+                  end
+
+                rescue MapError
+                  false
+                end
+              end
+
+              private
+
+              def fetch_loader(file_label)
+                case file_label
+                when 'PM'
+                  DataSource::File::PM::Operation::Load
+
+                # TODO add loader when implemented
+                when 'PP'
+                when 'rep'
+                when 'ets'
+                when 'obs'
+                when 'actes'
+                when 'comptes_annuels'
+
+                else
+                  raise MapError, "Error : unknown label `#{file_label}`."
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
