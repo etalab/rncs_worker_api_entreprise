616c66359cd889f25922363ee9412178263183cf
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-08-22 17:12:37 +0200

    Boyscoot

--- app/concepts/data_source/stock/tribunal_commerce/operation/retrieve_last_stock.rb
+++ app/concepts/data_source/stock/tribunal_commerce/operation/retrieve_last_stock.rb
@@ -3,27 +3,36 @@ module DataSource
     module TribunalCommerce
       module Operation
         class RetrieveLastStock < Trailblazer::Operation
-          RNCSSourceDir = '/Users/alexandre/Professional/temp_imr/IMR_Donnees_Saisies/tc/stock/'
+          RNCSSourceDir = '/Users/alexandre/Professional/temp_imr/IMR_Donnees_Saisies/IMR_Donnees_Saisies/tc/stock/'
 
-          step :fetch_last_stock_from_source_dir
+          step :retrieve_stocks
+          fail ->(ctx, **) { ctx[:errors] = "No stock found into #{RNCSSourceDir}" }, fail_fast: true
 
-          def fetch_last_stock_from_source_dir(ctx, **)
-            source_dir_stocks = Dir.glob("#{RNCSSourceDir}*/*/*")
-            stocks = deserialize_stocks(source_dir_stocks)
-            ctx[:stock] = stocks.max { |a, b| a.date <=> b.date }
+          step :deserialize_stocks
+          step :most_recent_stock
+
+
+          def retrieve_stocks(ctx, **)
+            stocks_paths = Dir.glob("#{RNCSSourceDir}*/*/*")
+            ctx[:stock_path_list] = stocks_paths unless stocks_paths.empty?
           end
 
-          def deserialize_stocks(fs_stock_list)
-            stocks = fs_stock_list.map do |dump|
-              if match = dump.match(/\A#{RNCSSourceDir}(.{4})\/(.{2})\/(.{2})\Z/)
+          # Stocks are located in subfolders following the 'AAAA/MM/DD' pattern
+          def deserialize_stocks(ctx, stock_path_list:, **)
+            stock_list = stock_path_list.map do |path|
+              if match = path.match(/\A#{RNCSSourceDir}(.{4})\/(.{2})\/(.{2})\Z/)
                 year, month, day = match.captures
-                StockTribunalCommerce.new(year: year, month: month, day: day, files_path: dump)
+                StockTribunalCommerce.new(year: year, month: month, day: day, files_path: path)
               else
                 # TODO manage errors
               end
             end
 
-            stocks
+            ctx[:stock_list] = stock_list
+          end
+
+          def most_recent_stock(ctx, stock_list:, **)
+            ctx[:stock] = stock_list.max { |a, b| a.date <=> b.date }
           end
         end
       end
