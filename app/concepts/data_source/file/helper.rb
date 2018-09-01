6691d037c83c38b941fff15ed0790eeb4ea30043
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-09-01 16:09:59 +0200

    First step renaming app/concepts/data_source/file/ subdirectories

--- app/concepts/data_source/file/helper.rb
+++
@@ -1,9 +0,0 @@
-module DataSource
-  module File
-    module Helper
-      def csv_to_hash(ctx, file_path:, **)
-        ::File.open(file_path, 'r:bom|utf-8') { |f| ctx[:raw_data] = SmarterCSV.process(f, { col_sep: ';', remove_empty_values: false }) }
-      end
-    end
-  end
-end
