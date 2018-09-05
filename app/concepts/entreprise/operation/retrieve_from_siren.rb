4ade7163eea2b1224c8b2de430fc0db188e7c116
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-09-05 11:39:12 +0200

    Boyscoot

--- app/concepts/entreprise/operation/retrieve_from_siren.rb
+++ app/concepts/entreprise/operation/retrieve_from_siren.rb
@@ -5,8 +5,10 @@ class Entreprise
 
 
       def find(ctx, params:, siren_in_db:, **)
-        _, entreprise_id = siren_in_db.assoc(params[:siren].to_s)
-        params[:entreprise_id] = entreprise_id
+        # https://docs.ruby-lang.org/en/2.0.0/Array.html#method-i-assoc :troll:
+        # siren_in_db => [[siren:, uid:], ...]
+        _, entreprise_uid = siren_in_db.assoc(params[:siren].to_s)
+        params[:entreprise_id] = entreprise_uid
       end
     end
   end
