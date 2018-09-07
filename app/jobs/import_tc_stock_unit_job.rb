12863182b93288d000f8af97ca34e368cf305c96
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-09-07 15:18:22 +0200

    Disable ActiveRecord cache on import

--- app/jobs/import_tc_stock_unit_job.rb
+++ app/jobs/import_tc_stock_unit_job.rb
@@ -2,6 +2,9 @@ class ImportTcStockUnitJob < ApplicationJob
   queue_as :tc_stock
 
   def perform(path:, **)
-    DataSource::Stock::TribunalCommerce::Unit::Operation::Load.call(stock_unit_path: path)
+    # See if disabling ActiveRecord cache free the sidekiq process memory
+    ActiveRecord::Base.uncached do
+      DataSource::Stock::TribunalCommerce::Unit::Operation::Load.call(stock_unit_path: path)
+    end
   end
 end
