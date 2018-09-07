1db9a09f3373b5e1adae754cf2dad1319f3a791f
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-09-07 15:29:48 +0200

    Give code_greffe: to the stock unit load operation

--- app/jobs/import_tc_stock_unit_job.rb
+++ app/jobs/import_tc_stock_unit_job.rb
@@ -1,10 +1,11 @@
 class ImportTcStockUnitJob < ApplicationJob
   queue_as :tc_stock
 
-  def perform(path:, **)
+  def perform(path:, code_greffe:, **)
     # See if disabling ActiveRecord cache free the sidekiq process memory
     ActiveRecord::Base.uncached do
-      DataSource::Stock::TribunalCommerce::Unit::Operation::Load.call(stock_unit_path: path)
+      DataSource::Stock::TribunalCommerce::Unit::Operation::Load
+        .call(stock_unit_path: path, code_greffe: code_greffe)
     end
   end
 end
