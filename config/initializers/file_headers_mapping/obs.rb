34548b8b16a6f0d18d89a5ea9e44f6db89da03fc
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-09-20 15:54:27 +0200

    Update Observation fields extraction from file

---
+++ config/initializers/file_headers_mapping/obs.rb
@@ -0,0 +1,13 @@
+OBS_HEADER_MAPPING = {
+  'Code Greffe' => :code_greffe,
+  'Nom_Greffe' => nil,
+  'Numero_Gestion' => :numero_gestion,
+  'Siren' => :siren,
+  'ID_Observation' => :id_observation,
+  'Numéro_Observation' => :numero,
+  'Date_Ajout' => :date_ajout,
+  'Date_Suppression' => :date_suppression,
+  'Texte' => :texte,
+  'Date_Greffe' => :date_derniere_modification,
+  'Etat' => :etat,
+}
---
+++ spec/concepts/data_source/file/csv_reader/obs_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe DataSource::File::CSVReader do
+  let(:fixtures_path) { Rails.root.join('spec', 'fixtures') }
+  let(:csv_file) { fixtures_path.join('obs.csv') }
+  let(:header_mapping) { OBS_HEADER_MAPPING }
+
+  subject do
+    raw_data = []
+    reader = described_class.new(csv_file, header_mapping)
+
+    reader.proceed do |batch|
+      raw_data = batch
+    end
+    raw_data.first
+  end
+
+  describe 'header keys transformations' do
+    it { is_expected.to have_key(:code_greffe) }
+    it { is_expected.to have_key(:numero_gestion) }
+    it { is_expected.to have_key(:siren) }
+    it { is_expected.to have_key(:id_observation) }
+    it { is_expected.to have_key(:numero) }
+    it { is_expected.to have_key(:date_ajout) }
+    it { is_expected.to have_key(:date_suppression) }
+    it { is_expected.to have_key(:texte) }
+    it { is_expected.to have_key(:date_derniere_modification) }
+    it { is_expected.to have_key(:etat) }
+  end
+
+  describe 'dossier entreprise attributes exclusion' do
+    # fields in previous specs only
+    its(:size) { is_expected.to eq(10) }
+  end
+end
