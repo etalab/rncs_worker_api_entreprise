c8610808389e504a7cab05a2d1c44a03c75123d8
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-09-24 18:56:42 +0200

    Add operation to create indexes for Greffe's primary key and foreign keys columns

---
+++ app/concepts/data_source/stock/tribunal_commerce/operation/post_import.rb
@@ -0,0 +1,39 @@
+module DataSource
+  module Stock
+    module TribunalCommerce
+      module Operation
+        class PostImport < Trailblazer::Operation
+          step :create_db_indexes
+          step :fill_in_foreign_keys
+
+
+          def create_db_indexes(ctx, **)
+            sql = 'CREATE INDEX IF NOT EXISTS index_dossier_entreprise_enregistrement_id ON dossiers_entreprises (code_greffe, numero_gestion, siren);'
+            ActiveRecord::Base.connection.execute(sql)
+          end
+
+          def fill_in_foreign_keys(ctx, **)
+            [
+              'personnes_morales',
+              'personnes_physiques',
+              'representants',
+              'etablissements',
+              'observations',
+            ]
+              .each do |t|
+              sql = <<-END_SQL
+              UPDATE #{t}
+              SET dossier_entreprise_id = dossiers_entreprises.id
+              FROM dossiers_entreprises
+              WHERE #{t}.code_greffe = dossiers_entreprises.code_greffe
+              AND #{t}.numero_gestion = dossiers_entreprises.numero_gestion
+              AND #{t}.siren = dossiers_entreprises.siren
+              END_SQL
+              ActiveRecord::Base.connection.execute(sql)
+            end
+          end
+        end
+      end
+    end
+  end
+end
