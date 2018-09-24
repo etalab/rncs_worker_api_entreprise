0dde49a57bc2669b2b170c37da7868b5e27d2897
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-09-24 17:02:24 +0200

    Update database cleaning operations

--- app/concepts/data_source/stock/tribunal_commerce/unit/operation/reset_database.rb
+++ app/concepts/data_source/stock/tribunal_commerce/unit/operation/reset_database.rb
@@ -6,76 +6,35 @@ module DataSource
           class ResetDatabase
             class << self
               def call(ctx, code_greffe:, **)
-                # Going my own way because of ActiveRecord :dependent option's performance
                 # Be careful to drop rows related to the corresponding greffe only
-
                 code_greffe = code_greffe.to_i.to_s
 
-                # Identite and Adresse models have polymorphic associations
-                [
-                  ['representants',       'Representant'],
-                  ['personnes_physiques', 'PersonnePhysique']
-                ]
-                  .each do |table, type|
-                  identites = Identite
-                    .joins("INNER JOIN #{table} ON identites.identifiable_id = #{table}.id
-                            INNER JOIN entreprises ON #{table}.entreprise_id = entreprises.id"
-                          )
-                    .where(identifiable_type: type)
-                    .where(entreprises: { code_greffe: code_greffe })
-                    .unscope(:order)
-
-                  identites.delete_all unless identites.empty?
-                end
-
-                [
-                  ['representants', 'Representant'],
-                  ['etablissements', 'Etablissement'],
-                  ['personnes_physiques', 'PersonnePhysique'],
-                ]
-                  .each do |table, type|
-                  adresses = Adresse
-                    .joins("INNER JOIN #{table} ON adresses.addressable_id = #{table}.id
-                            INNER JOIN entreprises ON #{table}.entreprise_id = entreprises.id"
-                          )
-                    .where(addressable_type: type)
-                    .where(entreprises: { code_greffe: code_greffe })
-                    .unscope(:order)
-
-                  adresses.delete_all unless adresses.empty?
-                end
-
                 Observation
-                  .joins(:entreprise)
-                  .where(entreprises: { code_greffe: code_greffe })
+                  .where(code_greffe: code_greffe)
                   .unscope(:order)
                   .delete_all
 
                 Representant
-                  .joins(:entreprise)
-                  .where(entreprises: { code_greffe: code_greffe })
+                  .where(code_greffe: code_greffe)
                   .unscope(:order)
                   .delete_all
 
                 Etablissement
-                  .joins(:entreprise)
-                  .where(entreprises: { code_greffe: code_greffe })
+                  .where(code_greffe: code_greffe)
                   .unscope(:order)
                   .delete_all
 
                 PersonneMorale
-                  .joins(:entreprise)
-                  .where(entreprises: { code_greffe: code_greffe })
+                  .where(code_greffe: code_greffe)
                   .unscope(:order)
                   .delete_all
 
                 PersonnePhysique
-                  .joins(:entreprise)
-                  .where(entreprises: { code_greffe: code_greffe })
+                  .where(code_greffe: code_greffe)
                   .unscope(:order)
                   .delete_all
 
-                Entreprise
+                DossierEntreprise
                   .where(code_greffe: code_greffe)
                   .unscope(:order)
                   .delete_all
--- lib/tasks/clean_db.rake
+++ lib/tasks/clean_db.rake
@@ -1,10 +1,8 @@
 task clean_db: :environment do
-  Identite.delete_all
-  Adresse.delete_all
   Etablissement.delete_all
   Representant.delete_all
   PersonnePhysique.delete_all
   PersonneMorale.delete_all
   Observation.delete_all
-  Entreprise.delete_all
+  DossierEntreprise.delete_all
 end
