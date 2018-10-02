254a0f243f70a1ef1b623dbf5cf3c6ed00e14476
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-10-02 20:05:01 +0200

    Boyscoot

--- spec/models/stock_unit_spec.rb
+++ spec/models/stock_unit_spec.rb
@@ -6,6 +6,7 @@ describe StockUnit do
     it { is_expected.to have_db_column(:code_greffe).of_type(:string) }
     it { is_expected.to have_db_column(:status).of_type(:string) }
     it { is_expected.to have_db_column(:number).of_type(:integer) }
+    it { is_expected.to have_db_column(:file_path).of_type(:string) }
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
 
