f1d326d36becffad789ac9b1a6235da9f9d38af3
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-10-03 12:30:42 +0200

    Add flow control around daily updates status on daily imports

--- spec/factories/dayly_update.rb
+++ spec/factories/dayly_update.rb
@@ -5,34 +5,34 @@ FactoryBot.define do
     day { '21' }
     files_path { '/not/here' }
 
-    factory :dayly_update_with_pending_units, class: DaylyUpdate do
-      after(:create) do |update|
-        create_list(:dayly_update_unit, 3, status: 'PENDING', dayly_update: update)
+    factory :dayly_update_tribunal_commerce, parent: :dayly_update, class: DaylyUpdateTribunalCommerce do
+      type { 'DaylyUpdateTribunalCommerce' }
+
+      factory :dayly_update_with_pending_units, class: DaylyUpdate do
+        after(:create) do |update|
+          create_list(:dayly_update_unit, 3, status: 'PENDING', dayly_update: update)
+        end
+      end
+
+      factory :dayly_update_with_one_loading_unit, class: DaylyUpdate do
+        after(:create) do |update|
+          create_list(:dayly_update_unit, 3, status: 'PENDING', dayly_update: update)
+          create(:dayly_update_unit, status: 'LOADING', dayly_update: update)
+        end
+      end
+
+      factory :dayly_update_with_one_error_unit, class: DaylyUpdate do
+        after(:create) do |update|
+          create_list(:dayly_update_unit, 3, status: 'PENDING', dayly_update: update)
+          create(:dayly_update_unit, status: 'ERROR', dayly_update: update)
+        end
+      end
+
+      factory :dayly_update_with_completed_units, class: DaylyUpdate do
+        after(:create) do |update|
+          create_list(:dayly_update_unit, 3, status: 'COMPLETED', dayly_update: update)
+        end
       end
     end
-
-    factory :dayly_update_with_one_loading_unit, class: DaylyUpdate do
-      after(:create) do |update|
-        create_list(:dayly_update_unit, 3, status: 'PENDING', dayly_update: update)
-        create(:dayly_update_unit, status: 'LOADING', dayly_update: update)
-      end
-    end
-
-    factory :dayly_update_with_one_error_unit, class: DaylyUpdate do
-      after(:create) do |update|
-        create_list(:dayly_update_unit, 3, status: 'PENDING', dayly_update: update)
-        create(:dayly_update_unit, status: 'ERROR', dayly_update: update)
-      end
-    end
-
-    factory :dayly_update_with_completed_units, class: DaylyUpdate do
-      after(:create) do |update|
-        create_list(:dayly_update_unit, 3, status: 'COMPLETED', dayly_update: update)
-      end
-    end
-  end
-
-  factory :dayly_update_tribunal_commerce, parent: :dayly_update, class: DaylyUpdateTribunalCommerce do
-    type { 'DaylyUpdateTribunalCommerce' }
   end
 end
