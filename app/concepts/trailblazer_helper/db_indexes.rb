6fa8112932a9713602c12e910f3b2067b1e3812b
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-10-10 12:55:41 +0200

    Add config files to reference DB indexes with an helper to deserialize it

---
+++ app/concepts/trailblazer_helper/db_indexes.rb
@@ -0,0 +1,65 @@
+module TrailblazerHelper
+  module DBIndexes
+    def create_queries
+      queries = []
+      for_all_indexes do |table, index|
+        index_name = name_for(table, index)
+        index_columns = columns_for(index)
+        queries.push("CREATE INDEX IF NOT EXISTS #{index_name} ON #{table} #{index_columns};")
+      end
+
+      queries
+    end
+
+    def drop_queries
+      queries = []
+      for_all_indexes do |table, index|
+        index_name = name_for(table, index)
+        queries.push("DROP INDEX IF EXISTS #{index_name};")
+      end
+
+      queries
+    end
+
+
+    private
+
+    def configured_indexes
+      Rails.application.config_for(:db_indexes)
+    end
+
+    def for_all_indexes
+      configured_indexes.each do |table, indexes|
+        indexes.each do |index|
+          yield(table, index)
+        end
+      end
+    end
+
+    def name_for(table, index)
+      index_name = ''
+      if multi_columns?(index)
+        index_name = "index_#{table}_#{index.join('_')}"
+      else
+        index_name = "index_#{table}_#{index}"
+      end
+
+      index_name
+    end
+
+    def columns_for(index)
+      columns = ''
+      if multi_columns?(index)
+        columns = "(#{index.join(',')})"
+      else
+        columns = "(#{index})"
+      end
+
+      columns
+    end
+
+    def multi_columns?(index)
+      index.class == Array
+    end
+  end
+end
