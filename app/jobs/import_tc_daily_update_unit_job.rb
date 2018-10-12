e45e379e457c5c00d5cd24ff1e97cb69673ad16e
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-10-12 10:48:22 +0200

    Ensure DB writes are rollbacked when the daily unit import fails

--- app/jobs/import_tc_daily_update_unit_job.rb
+++ app/jobs/import_tc_daily_update_unit_job.rb
@@ -2,14 +2,23 @@ class ImportTcDailyUpdateUnitJob < ApplicationJob
   queue_as :tc_daily_update
 
   def perform(daily_update_id)
+    import = nil
     unit = DailyUpdateUnit.find(daily_update_id)
-    import = TribunalCommerce::DailyUpdateUnit::Operation::Load
-      .call(daily_update_unit: unit)
+    ActiveRecord::Base.transaction do
+      import = TribunalCommerce::DailyUpdateUnit::Operation::Load
+        .call(daily_update_unit: unit)
 
+      if import.success?
+        unit.update(status: 'COMPLETED')
+      else
+        raise ActiveRecord::Rollback
+      end
+    end
+
+    # Checking the import result a second time outside the transaction
+    # so the 'ERROR' status is persisted into DB and not rollback to 'PENDING'
     if import.success?
-      unit.update(status: 'COMPLETED')
       TribunalCommerce::DailyUpdateUnit::Operation::PostImport.call
-
     else
       unit.update(status: 'ERROR')
     end
--- spec/jobs/import_tc_daily_update_unit_job_spec.rb
+++ spec/jobs/import_tc_daily_update_unit_job_spec.rb
@@ -5,17 +5,18 @@ describe ImportTcDailyUpdateUnitJob do
   let(:unit) { create(:daily_update_unit, status: 'PENDING') }
   subject { described_class.perform_now(unit.id) }
 
-  before do
+  def mock_unit_load_operation(result)
     expect(TribunalCommerce::DailyUpdateUnit::Operation::Load)
       .to receive(:call)
       .with({ daily_update_unit: unit })
-      .and_return(operation_result)
+      .and_return(result)
   end
 
   context 'when the unit import is successful' do
     let(:operation_result) { instance_double(result_class, success?: true) }
 
     it 'sets the unit\'s status to "COMPLETED"' do
+      mock_unit_load_operation(operation_result)
       subject
       unit.reload
 
@@ -23,6 +24,7 @@ describe ImportTcDailyUpdateUnitJob do
     end
 
     it 'calls PostImport' do
+      mock_unit_load_operation(operation_result)
       expect(TribunalCommerce::DailyUpdateUnit::Operation::PostImport)
         .to receive(:call)
 
@@ -33,7 +35,22 @@ describe ImportTcDailyUpdateUnitJob do
   context 'when the unit import fails' do
     let(:operation_result) { instance_double(result_class, success?: false) }
 
+    it 'rollbacks everything written in database' do
+      expect(TribunalCommerce::DailyUpdateUnit::Operation::Load)
+        .to receive(:call)
+        .and_wrap_original do |original_method, *args|
+          create(:daily_update_unit, status: 'GHOST')
+          operation_result
+        end
+
+      subject
+
+      ghost = DailyUpdateUnit.where(status: 'GHOST')
+      expect(ghost).to be_empty
+    end
+
     it 'sets the unit\'s status to "ERROR"' do
+      mock_unit_load_operation(operation_result)
       subject
       unit.reload
 
