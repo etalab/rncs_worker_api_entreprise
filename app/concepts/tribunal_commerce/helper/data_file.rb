fdd60c8b62faaea777e74c7180d258697967daa3
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-10-17 19:38:19 +0200

    Add an helper to read files from the pipe

---
+++ app/concepts/tribunal_commerce/helper/data_file.rb
@@ -0,0 +1,92 @@
+module TribunalCommerce
+  module Helper
+    module DataFile
+      class UnknownFileLabel < StandardError; end
+      class UnexpectedFilename < StandardError; end
+
+      def fetch_from_folder(folder_path)
+        pattern = ::File.join(folder_path, '*.csv')
+        Dir.glob(pattern)
+      end
+
+      def parse_flux_filename(file_list_paths)
+        data = file_list_paths.map! do |file_path|
+          filename = file_path.split('/').last
+
+          if match = filename.match(flux_filename_regex)
+            code_greffe, num_transmission, date, hour, run_order, label = match.captures
+
+            {
+              code_greffe: code_greffe,
+              run_order: run_order.to_i,
+              label: label,
+              path: file_path
+            }
+          else
+            raise UnexpectedFilename, "Cannot parse filename : \"#{filename}\" does not match the expected pattern"
+          end
+        end
+        data
+      end
+
+      def map_import_worker(files_args)
+        files_args.map! do |file_arg|
+          file_arg[:import_worker] = fetch_worker_for(file_arg[:label])
+          file_arg
+        end
+      end
+
+      private
+
+      def capture_metadata(filename)
+      end
+
+      def flux_filename_regex
+        /\A(\d{4})_(\d{1,})_(\d{8})_(\d{6})_(\d{1,2})_(.+)\.csv\Z/
+      end
+
+      def fetch_worker_for(label)
+        case label
+        when 'PM'
+          DataSource::File::PM::Operation::Import
+
+        when 'PM_EVT'
+          TribunalCommerce::File::PMEvent::Operation::Import
+
+        when 'PP'
+          DataSource::File::PP::Operation::Import
+
+        when 'PP_EVT'
+          TribunalCommerce::File::PPEvent::Operation::Import
+
+        when 'rep'
+          DataSource::File::Rep::Operation::Import
+
+        when 'rep_nouveau_modifie_EVT'
+          TribunalCommerce::File::RepNouveauModifie::Operation::Import
+
+        when 'rep_partant_EVT'
+          TribunalCommerce::File::RepPartant::Operation::Import
+
+        when 'ets'
+          DataSource::File::Ets::Operation::Import
+
+        when 'ets_nouveau_modifie_EVT'
+          TribunalCommerce::File::EtsNouveauModifie::Operation::Import
+
+        when 'ets_supprime_EVT'
+          TribunalCommerce::File::EtsSupprime::Operation::Import
+
+        when 'obs'
+          DataSource::File::Obs::Operation::Import
+
+        when 'actes'
+        when 'comptes_annuels'
+
+        else
+          raise UnknownFileLabel, "Unknown file label \"#{label}\""
+        end
+      end
+    end
+  end
+end
---
+++ spec/concepts/tribunal_commerce/helper/data_file_spec.rb
@@ -0,0 +1,113 @@
+require 'rails_helper'
+
+describe TribunalCommerce::Helper::DataFile do
+  let(:includer) do
+    class A
+      include TribunalCommerce::Helper::DataFile
+    end
+    A.new
+  end
+
+  describe '#fetch_from_folder' do
+    it 'returns absolute files path inside the folder'
+  end
+
+  describe '#parse_stock_filename' do
+
+  end
+
+  describe '#parse_flux_filename' do
+    subject { includer.parse_flux_filename(["/random/path/#{filename}"]) }
+
+    context 'with a valid filename' do
+      let(:filename) { '0101_1_20170512_112544_6_rep_nouveau_modifie_EVT.csv' }
+
+      it 'reads the code greffe' do
+        expect(subject).to contain_exactly(a_hash_including(code_greffe: '0101'))
+      end
+
+      it 'reads the running order' do
+        expect(subject).to contain_exactly(a_hash_including(run_order: 6))
+      end
+
+      it 'reads the label' do
+        expect(subject).to contain_exactly(a_hash_including(label: 'rep_nouveau_modifie_EVT'))
+      end
+    end
+
+    context 'with an invalid filename' do
+      let(:filename) { 'inval1d_filename.txt' }
+
+      it 'raises UnexpectedFilename' do
+        expect { subject }
+          .to raise_error(
+            TribunalCommerce::Helper::DataFile::UnexpectedFilename,
+            'Cannot parse filename : "inval1d_filename.txt" does not match the expected pattern'
+          )
+      end
+    end
+  end
+
+  describe '#map_import_worker' do
+    def worker(label)
+      result = includer.map_import_worker([{ label: label }])
+      result.first.fetch(:import_worker)
+    end
+
+    context 'when label is known' do
+      it 'maps PM file' do
+        expect(worker('PM')).to eq(DataSource::File::PM::Operation::Import)
+      end
+
+      it 'maps PM_EVT file' do
+        expect(worker('PM_EVT')).to eq(TribunalCommerce::File::PMEvent::Operation::Import)
+      end
+
+      it 'maps PP file' do
+        expect(worker('PP')).to eq(DataSource::File::PP::Operation::Import)
+      end
+
+      it 'maps PP_EVT file' do
+        expect(worker('PP_EVT')).to eq(TribunalCommerce::File::PPEvent::Operation::Import)
+      end
+
+      it 'maps rep file' do
+        expect(worker('rep')).to eq(DataSource::File::Rep::Operation::Import)
+      end
+
+      it 'maps rep_nouveau_modifie_EVT file' do
+        expect(worker('rep_nouveau_modifie_EVT')).to eq(TribunalCommerce::File::RepNouveauModifie::Operation::Import)
+      end
+
+      it 'maps rep_partant_EVT file' do
+        expect(worker('rep_partant_EVT')).to eq(TribunalCommerce::File::RepPartant::Operation::Import)
+      end
+
+      it 'maps ets file' do
+        expect(worker('ets')).to eq(DataSource::File::Ets::Operation::Import)
+      end
+
+      it 'maps ets_nouveau_modifie_EVT file' do
+        expect(worker('ets_nouveau_modifie_EVT')).to eq(TribunalCommerce::File::EtsNouveauModifie::Operation::Import)
+      end
+
+      it 'maps ets_supprime_EVT file' do
+        expect(worker('ets_supprime_EVT')).to eq(TribunalCommerce::File::EtsSupprime::Operation::Import)
+      end
+
+      it 'maps obs file' do
+        expect(worker('obs')).to eq(DataSource::File::Obs::Operation::Import)
+      end
+    end
+
+    context 'when label is unknown' do
+      it 'raises UnknownFileLabel exception with an unknown label' do
+        expect { worker('invalid label') }
+          .to raise_error(
+            TribunalCommerce::Helper::DataFile::UnknownFileLabel,
+            'Unknown file label "invalid label"'
+        )
+      end
+    end
+  end
+end
