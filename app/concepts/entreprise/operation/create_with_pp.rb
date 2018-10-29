91963eba99b9529f6c173a924f04632fdefd812d
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-10-29 17:25:38 +0100

    Boyscoot

--- app/concepts/entreprise/operation/create_with_pp.rb
+++
@@ -1,9 +0,0 @@
-module Entreprise
-  module Operation
-    class CreateWithPP < Trailblazer::Operation
-      step Model(DossierEntreprise, :new)
-      step Trailblazer::Operation::Contract::Validate()
-      step Trailblazer::Operation::Contract::Persist(method: :sync)
-    end
-  end
-end
