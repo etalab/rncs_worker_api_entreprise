39e42f524640e8b8400b7f811e5f4ae0f9987760
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-10-29 18:18:27 +0100

    Add obs file stock and flux import

--- app/concepts/data_source/file/obs/operation/import.rb
+++ app/concepts/data_source/file/obs/operation/import.rb
@@ -3,14 +3,35 @@ module DataSource
     module Obs
       module Operation
         class Import < Trailblazer::Operation
-          step ->(ctx, file_path:, **) do
-            csv_reader = File::CSVReader.new(
-              file_path,
-              OBS_HEADER_MAPPING)
+          extend ClassDependencies
 
-            csv_reader.proceed do |batch|
-              Observation.import batch, validate: false
+          self[:file_reader] = DataSource::File::CSVReader
+
+          step ->(ctx, type_import:, **) { [:stock, :flux].include?(type_import) }
+            fail :invalid_type_import, fail_fast: true
+
+          step ->(ctx, type_import:, **) { type_import == :stock }
+            fail :line_import, Output(:success) => 'End.success'
+          step :mass_import
+
+
+          def mass_import(ctx, file_path:, file_reader:, **)
+            mapping = OBS_HEADER_MAPPING
+            file_reader.bulk_processing(file_path, mapping) do |batch|
+              Observation.import(batch)
             end
+          end
+
+          def line_import(ctx, file_path:, file_reader:, **)
+            file_reader.line_processing(file_path, OBS_HEADER_MAPPING) do |line|
+              line_import = Observation::Operation::Create.call(data: line)
+
+              return false if line_import.failure?
+            end
+          end
+
+          def invalid_type_import(ctx, logger:, type_import:, **)
+            logger.error("Invalid call for file import : import type :#{type_import} is unknown.")
           end
         end
       end
--- spec/concepts/data_source/file/obs/operation/import_spec.rb
+++ spec/concepts/data_source/file/obs/operation/import_spec.rb
@@ -1,12 +1,37 @@
 require 'rails_helper'
 
-describe DataSource::File::Obs::Operation::Import do
-  let(:file_path) { Rails.root.join('spec', 'fixtures', 'obs.csv') }
+describe DataSource::File::Obs::Operation::Import, :trb do
+  file = Rails.root.join('spec', 'fixtures', 'obs.csv')
 
-  subject { described_class.call(file_path: file_path) }
+  subject { described_class.call(file_path: file, type_import: type_import) }
 
-  it 'saves Observation records' do
-    pending 'fix this spec since I can\'t let an empty space at the end of the line because of vim'
-    expect{ subject }.to change(Observation, :count).by(5)
+  context 'when type_import: :stock' do
+    let(:type_import) { :stock }
+
+    it_behaves_like 'bulk import', Observation, file, OBS_HEADER_MAPPING
+
+    it { is_expected.to be_success }
+  end
+
+  context 'when type_import: :flux' do
+    let(:type_import) { :flux }
+
+    it_behaves_like 'line import',
+      Observation::Operation::Create,
+      Etablissement,
+      file,
+      OBS_HEADER_MAPPING
+
+    it 'is success' do
+      allow(Observation::Operation::Create)
+        .to receive(:call)
+        .and_return(trb_result_success)
+
+      expect(subject).to be_success
+    end
+  end
+
+  context 'when :type_import is unknown' do
+    it_behaves_like 'invalid import type'
   end
 end
