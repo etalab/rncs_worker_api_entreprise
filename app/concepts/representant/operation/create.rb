cc3a35391bb424859d5c8ad516f9f9f95ce23c82
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-10-30 10:47:07 +0100

    Add operation to save representants one by one from stock file

--- app/concepts/representant/operation/create.rb
+++ app/concepts/representant/operation/create.rb
@@ -1,7 +1,25 @@
 class Representant
   module Operation
     class Create < Trailblazer::Operation
+      step :find_dossier_entreprise
+        fail :dossier_not_found
+      step :save_representant
 
+
+      def find_dossier_entreprise(ctx, data:, **)
+        ctx[:dossier] = DossierEntreprise.find_by({
+          code_greffe: data[:code_greffe],
+          numero_gestion: data[:numero_gestion],
+        })
+      end
+
+      def save_representant(ctx, dossier:, data:, **)
+        ctx[:created_rep] = dossier.representants.create(data)
+      end
+
+      def dossier_not_found(ctx, data:, **)
+        ctx[:error] = "The dossier (code_greffe: #{data[:code_greffe]}, numero_gestion: #{data[:numero_gestion]}) is not found. The representant cannot be save"
+      end
     end
   end
 end
---
+++ spec/concepts/representant/operation/create_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe Representant::Operation::Create do
+  let(:rep_data) do
+    {
+      code_greffe: '9001',
+      numero_gestion: '2016A10937',
+      qualite: 'Bullshit analyst',
+      prenoms: 'Jean Peuplu'
+    }
+  end
+
+  subject { described_class.call(data: rep_data) }
+
+  context 'when associated dossier entreprise exists' do
+    let(:created_rep) { subject[:created_rep] }
+
+    before { create(:dossier_entreprise, code_greffe: '9001', numero_gestion: '2016A10937') }
+
+    it { is_expected.to be_success }
+
+    it 'saves the representant' do
+      expect(created_rep).to be_persisted
+      expect(created_rep).to have_attributes(
+        code_greffe: '9001',
+        numero_gestion: '2016A10937',
+        qualite: 'Bullshit analyst',
+        prenoms: 'Jean Peuplu',
+      )
+    end
+
+    it 'associates the representant to the correct dossier' do
+      dossier = DossierEntreprise.find_by(code_greffe: '9001', numero_gestion: '2016A10937')
+
+      expect(created_rep.dossier_entreprise).to eq(dossier)
+    end
+  end
+
+  context 'when associated dossier entreprise is not found' do
+    it { is_expected.to be_failure }
+
+    it 'returns an error message' do
+      error_msg = subject[:error]
+
+      expect(error_msg).to eq('The dossier (code_greffe: 9001, numero_gestion: 2016A10937) is not found. The representant cannot be save')
+    end
+  end
+end
