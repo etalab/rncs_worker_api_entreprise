a090e1e2680b10786ed6e4dcba9b26a0c4a9b05b
Hugo Lepetit <giglemad@gmail.com>
2018-11-01 14:48:53 +0100

    Added domain to logstasher fields

--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -1,6 +1,7 @@
 if LogStasher.enabled
   LogStasher.add_custom_fields do |fields|
     fields[:type] = 'rncs'
+    fields[:domain] = request.domain
   end
 
   LogStasher.add_custom_fields_to_request_context do |fields|
