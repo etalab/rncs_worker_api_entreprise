4376afe7e531454b2542feb168b35ea16c6bc270
Hugo Lepetit <giglemad@gmail.com>
2018-11-01 10:30:33 +0100

    Removed /api\/*_DATA_SOURCE/ formalism. Nginx will take care of this

--- spec/requests/cors_requests_spec.rb
+++ spec/requests/cors_requests_spec.rb
@@ -7,7 +7,7 @@ describe 'Cors requests' do
 
   # More on testing cors policy : http://jbilbo.com/blog/2015/05/19/testing-cors-with-rspec/
   it 'allows any origin for /api/infos_identite_entreprise_rncs' do
-    get "/api/infos_identite_entreprise_rncs/#{valid_siren}", headers: { 'HTTP_ORIGIN': '*' }
+    get "/infos_identite_entreprise/#{valid_siren}", headers: { 'HTTP_ORIGIN': '*' }
 
     expect(response.headers['Access-Control-Allow-Origin']).to eq('*')
   end
