a0464f45627465baec34297510d21f5ca67d6984
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-11-12 15:36:39 +0100

    Delete representants listed into rep_partant_EVT.csv files if found
    As noticed into the official documentation, leaving representant may not
    be found in base (?). This behaviour is usual and should not be
    considered as an error (...). We log a warning message when this happen.

--- app/concepts/representant/operation/delete.rb
+++ app/concepts/representant/operation/delete.rb
@@ -1,7 +1,27 @@
 class Representant
   module Operation
     class Delete < Trailblazer::Operation
+      step :find_representant
+        fail :warning_message, Output(:success) => 'End.success'
+      step :delete
 
+
+      def find_representant(ctx, data:, **)
+        ctx[:representant] = Representant.find_by(
+          code_greffe: data[:code_greffe],
+          numero_gestion: data[:numero_gestion],
+          id_representant: data[:id_representant],
+          qualite: data[:qualite],
+        )
+      end
+
+      def delete(ctx, representant:, **)
+        representant.delete
+      end
+
+      def warning_message(ctx, data:, **)
+        ctx[:warning] = "The representant with id (code_greffe: #{data[:code_greffe]}, numero_gestion: #{data[:numero_gestion]}, id_representant: #{data[:id_representant]}, qualite: #{data[:qualite]}) does not exist in the database and cannot be deleted"
+      end
     end
   end
 end
