238418c0ebf965e58db2af630013381dc475b3a4
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-11-12 11:59:16 +0100

    Add an operation to update representants or create them if they do not exist

--- app/concepts/representant/operation/nouveau_modifie.rb
+++ app/concepts/representant/operation/nouveau_modifie.rb
@@ -1,6 +1,43 @@
 class Representant
   module Operation
     class NouveauModifie < Trailblazer::Operation
+      step :find_dossier_entreprise
+        fail :dossier_not_found, fail_fast: true
+      step :retrieve_representant
+        fail :create_new_representant
+        fail :warning_message, Output(:success) => 'End.success'
+      step :update
+
+
+      def find_dossier_entreprise(ctx, data:, **)
+        ctx[:dossier] = DossierEntreprise.find_by({
+          code_greffe: data[:code_greffe],
+          numero_gestion: data[:numero_gestion]
+        })
+      end
+
+      def retrieve_representant(ctx, dossier:, data:, **)
+        ctx[:representant] = dossier.representants.find_by(
+          id_representant: data[:id_representant],
+          qualite: data[:qualite]
+        )
+      end
+
+      def update(ctx, representant:, data:, **)
+        representant.update_attributes(data)
+      end
+
+      def create_new_representant(ctx, dossier:, data:, **)
+        dossier.representants.create(data)
+      end
+
+      def warning_message(ctx, data:, **)
+        ctx[:warning] = "The representant (id_representant: #{data[:id_representant]}, qualite: #{data[:qualite]}) was not found in dossier (code_greffe: #{data[:code_greffe]}, numero_gestion: #{data[:numero_gestion]}). Representant created instead."
+      end
+
+      def dossier_not_found(ctx, data:, **)
+        ctx[:error] = "The dossier (code_greffe: #{data[:code_greffe]}, numero_gestion: #{data[:numero_gestion]}) is not found. Aborting..."
+      end
     end
   end
 end
---
+++ spec/concepts/representant/operation/nouveau_modifie_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+describe Representant::Operation::NouveauModifie do
+  context 'when the related dossier is not found' do
+    it_behaves_like 'related dossier not found'
+  end
+
+  let(:data) do
+    {
+      code_greffe: '1234',
+      numero_gestion: '1A2B3C',
+      id_representant: '1',
+      qualite: 'Président',
+      denomination: 'Holding my beer',
+      siren_pm: '123456789',
+      representant_permanent_adresse_ligne_1: '23 rue bidon',
+    }
+  end
+
+  subject { described_class.call(data: data) }
+
+  context 'when the dossier exists' do
+    let(:dossier) do
+      create(
+        :dossier_entreprise,
+        code_greffe: '1234',
+        numero_gestion: '1A2B3C',
+      )
+    end
+
+    context 'when the representant exists' do
+      before do
+        create(:representant, id_representant: '1', qualite: 'Président', dossier_entreprise: dossier)
+      end
+
+      it { is_expected.to be_success }
+
+      it 'updates the representant' do
+        subject
+        updated_rep = dossier.representants.find_by(id_representant: '1', qualite: 'Président')
+
+        expect(updated_rep).to have_attributes(
+          denomination: 'Holding my beer',
+          siren_pm: '123456789',
+          representant_permanent_adresse_ligne_1: '23 rue bidon'
+        )
+      end
+    end
+
+    context 'when the representant does not exist' do
+      before { dossier }
+
+      it { is_expected.to be_success }
+
+      it 'creates a new representant associated with the dossier' do
+        expect { subject }.to change(dossier.representants, :count).by(1)
+      end
+
+      it 'returns a warning message' do
+        warning_msg = subject[:warning]
+
+        expect(warning_msg).to eq('The representant (id_representant: 1, qualite: Président) was not found in dossier (code_greffe: 1234, numero_gestion: 1A2B3C). Representant created instead.')
+      end
+    end
+  end
+end
