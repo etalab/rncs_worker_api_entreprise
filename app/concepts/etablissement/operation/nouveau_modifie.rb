6f2f906956c119dec05b42f95b4e3ae67619d472
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-11-12 18:22:57 +0100

    Update etablissement or create them if not found in db

--- app/concepts/etablissement/operation/nouveau_modifie.rb
+++ app/concepts/etablissement/operation/nouveau_modifie.rb
@@ -1,7 +1,40 @@
 class Etablissement
   module Operation
     class NouveauModifie < Trailblazer::Operation
+      step :find_dossier_entreprise
+        fail :dossier_not_found, fail_fast: true
+      step :retrieve_etablissement
+        fail :create_new_etablissement
+        fail :warning_message, Output(:success) => 'End.success'
+      step :update
 
+
+      def find_dossier_entreprise(ctx, data:, **)
+        ctx[:dossier] = DossierEntreprise.find_by({
+          code_greffe: data[:code_greffe],
+          numero_gestion: data[:numero_gestion]
+        })
+      end
+
+      def retrieve_etablissement(ctx, dossier:, data:, **)
+        ctx[:etablissement] = dossier.etablissements.find_by(id_etablissement: data[:id_etablissement])
+      end
+
+      def update(ctx, etablissement:, data:, **)
+        etablissement.update_attributes(data)
+      end
+
+      def create_new_etablissement(ctx, dossier:, data:, **)
+        dossier.etablissements.create(data)
+      end
+
+      def warning_message(ctx, data:, **)
+        ctx[:warning] = "The etablissement (id_etablissement: #{data[:id_etablissement]}) was not found in dossier (code_greffe: #{data[:code_greffe]}, numero_gestion: #{data[:numero_gestion]}) for update. Created instead."
+      end
+
+      def dossier_not_found(ctx, data:, **)
+        ctx[:error] = "The dossier (code_greffe: #{data[:code_greffe]}, numero_gestion: #{data[:numero_gestion]}) is not found. Aborting..."
+      end
     end
   end
 end
---
+++ spec/concepts/etablissement/operation/nouveau_modifie_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+describe Etablissement::Operation::NouveauModifie do
+  context 'when the related dossier is not found' do
+    it_behaves_like 'related dossier not found'
+  end
+
+  let(:data) do
+    {
+      code_greffe: '1234',
+      numero_gestion: '1A2B3C',
+      id_etablissement: '1',
+      nom_commercial: 'GG EASY',
+      enseigne: 'YOU SALTY',
+    }
+  end
+
+  subject { described_class.call(data: data) }
+
+  context 'when the dossier exists' do
+    let(:dossier) do
+      create(
+        :dossier_entreprise,
+        code_greffe: '1234',
+        numero_gestion: '1A2B3C',
+      )
+    end
+
+    context 'when the etablissement exists' do
+      before do
+        create(:etablissement, id_etablissement: '1', dossier_entreprise: dossier)
+      end
+
+      it { is_expected.to be_success }
+
+      it 'updates the etablissement' do
+        subject
+        updated_ets = dossier.etablissements.find_by(id_etablissement: '1')
+
+        expect(updated_ets).to have_attributes(
+          nom_commercial: 'GG EASY',
+          enseigne: 'YOU SALTY',
+        )
+      end
+    end
+
+    context 'when the etablissement does not exist' do
+      before { dossier }
+
+      it 'creates a new etablissement associated with the dossier' do
+        expect { subject }.to change(dossier.etablissements, :count).by(1)
+      end
+
+      it 'returns a warning message' do
+        warning_msg = subject[:warning]
+
+        expect(warning_msg).to eq('The etablissement (id_etablissement: 1) was not found in dossier (code_greffe: 1234, numero_gestion: 1A2B3C) for update. Created instead.')
+      end
+
+      it { is_expected.to be_success }
+    end
+  end
+end
