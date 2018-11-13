427a80d9379e157a6e245359e760c084a00c39c8
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-11-13 15:29:26 +0100

    Delete etablissement referenced into ets_supprime_EVT.csv files

--- app/concepts/etablissement/operation/delete.rb
+++ app/concepts/etablissement/operation/delete.rb
@@ -1,7 +1,26 @@
 class Etablissement
   module Operation
     class Delete < Trailblazer::Operation
+      step :find_etablissement
+        fail :warning_message, Output(:success) => 'End.success'
+      step :delete
 
+
+      def find_etablissement(ctx, data:, **)
+        ctx[:etablissement] = Etablissement.find_by(
+          code_greffe: data[:code_greffe],
+          numero_gestion: data[:numero_gestion],
+          id_etablissement: data[:id_etablissement],
+        )
+      end
+
+      def delete(ctx, etablissement:, **)
+        etablissement.delete
+      end
+
+      def warning_message(ctx, data:, **)
+        ctx[:warning] = "The etablissement with id (code_greffe: #{data[:code_greffe]}, numero_gestion: #{data[:numero_gestion]}, id_etablissement: #{data[:id_etablissement]}) does not exist in the database and cannot be deleted"
+      end
     end
   end
 end
---
+++ spec/concepts/etablissement/operation/delete_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe Etablissement::Operation::Delete do
+  let(:data) do
+    {
+      code_greffe: '1234',
+      numero_gestion: '1A2B3C',
+      id_etablissement: '1',
+    }
+  end
+
+  subject { described_class.call(data: data) }
+
+  context 'when the etablissement exists in database' do
+    before do
+      create(
+        :etablissement,
+        code_greffe: '1234',
+        numero_gestion: '1A2B3C',
+        id_etablissement: '1',
+      )
+    end
+
+    it 'is deleted' do
+      expect { subject }.to change(Etablissement, :count).by(-1)
+    end
+
+    it { is_expected.to be_success }
+  end
+
+  context 'when the etablissement does not exist' do
+    it 'returns a warning message' do
+      warning_msg = subject[:warning]
+
+      expect(warning_msg).to eq('The etablissement with id (code_greffe: 1234, numero_gestion: 1A2B3C, id_etablissement: 1) does not exist in the database and cannot be deleted')
+    end
+
+    it { is_expected.to be_success }
+  end
+end
