5fa98b4bbef42878f58505a9f274c554f89e94b5
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-11-13 15:21:25 +0100

    Import ets_supprime_EVT.csv files

---
+++ app/concepts/etablissement/operation/delete.rb
@@ -0,0 +1,7 @@
+class Etablissement
+  module Operation
+    class Delete < Trailblazer::Operation
+
+    end
+  end
+end
--- app/concepts/tribunal_commerce/file/ets_supprime/operation/import.rb
+++ app/concepts/tribunal_commerce/file/ets_supprime/operation/import.rb
@@ -3,7 +3,24 @@ module TribunalCommerce
     module EtsSupprime
       module Operation
         class Import < Trailblazer::Operation
+          extend ClassDependencies
 
+          self[:file_reader] = DataSource::File::CSVReader
+
+          step :delete
+
+
+          def delete(ctx, file_reader:, file_path:, logger:, **)
+            mapping = ETS_HEADER_MAPPING
+            file_reader.line_processing(file_path, mapping) do |line|
+              delete_ets = Etablissement::Operation::Delete.call(data: line)
+
+              if delete_ets.failure?
+                logger.error(delete_ets[:error])
+                return false
+              end
+            end
+          end
         end
       end
     end
