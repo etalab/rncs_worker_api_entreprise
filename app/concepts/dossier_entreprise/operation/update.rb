3370d70aa5ccae8db3e0fdf7e0176d31fdc2c39a
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-11-22 14:55:03 +0100

    Don't strictly fail on dossier update if its not found
    Sometimes given PM or PP for updates do not exist in the stock... So we
    now create them instead. See issue #36 for more details.
    Therefore numero_gestion (which is part of the key to identify the
    dossier to update) can change (in case of forme_juridique change for
    example) so we now have a way to update it.

--- app/concepts/dossier_entreprise/operation/update.rb
+++ app/concepts/dossier_entreprise/operation/update.rb
@@ -2,8 +2,15 @@ class DossierEntreprise
   module Operation
     class Update < Trailblazer::Operation
       step :find_dossier_entreprise
-        fail :dossier_not_found
-      step :update
+        fail :find_dossier_with_same_siren,
+          Output(:success) => Track(:num_gestion_change),
+          Output(:failure) => Track(:new_dossier)
+
+        step :create_new_dossier, magnetic_to: [:new_dossier], Output(:success) => Track(:new_dossier)
+        step :warn_new_dossier, magnetic_to: [:new_dossier], Output(:success) => 'End.success'
+
+      step :warn_num_gestion_change, magnetic_to: [:num_gestion_change], Output(:success) => :update
+      step :update, id: :update
 
 
       def find_dossier_entreprise(ctx, data:, **)
@@ -17,8 +24,23 @@ class DossierEntreprise
         dossier.update_attributes(data)
       end
 
-      def dossier_not_found(ctx, data:, **)
-        ctx[:error] = "The dossier (code_greffe: #{data[:code_greffe]}, numero_gestion: #{data[:numero_gestion]}) is not found. Aborting..."
+      def find_dossier_with_same_siren(ctx, data:, **)
+        ctx[:dossier] = DossierEntreprise.find_by({
+          code_greffe: data[:code_greffe],
+          siren: data[:siren]
+        })
+      end
+
+      def create_new_dossier(ctx, data:, **)
+        DossierEntreprise.create(data)
+      end
+
+      def warn_new_dossier(ctx, data:, **)
+        ctx[:warning] = "Dossier (numero_gestion: #{data[:numero_gestion]}) not found and no dossier with the siren number #{data[:siren]} exist for greffe #{data[:code_greffe]}. Creating new dossier."
+      end
+
+      def warn_num_gestion_change(ctx, dossier:, data:, **)
+        ctx[:warning] = "Dossier (numero_gestion: #{data[:numero_gestion]}) not found for greffe #{data[:code_greffe]}, but an existing dossier (numero_gestion: #{dossier.numero_gestion}) is found for siren #{data[:siren]} : updating this dossier instead."
       end
     end
   end
--- spec/concepts/dossier_entreprise/operation/update_spec.rb
+++ spec/concepts/dossier_entreprise/operation/update_spec.rb
@@ -5,6 +5,7 @@ describe DossierEntreprise::Operation::Update do
     {
       code_greffe: '1234',
       numero_gestion: '1A2B3C',
+      siren: '123456789',
       date_immatriculation: 'hier',
       date_transfert: 'demain',
       sans_activite: 'OUI',
@@ -15,7 +16,55 @@ describe DossierEntreprise::Operation::Update do
   subject { described_class.call(data: data) }
 
   context 'when the dossier is not found' do
-    it_behaves_like 'related dossier not found'
+    # TODO https://github.com/etalab/rncs_worker_api_entreprise/issues/36
+    context 'with no existing dossier found for the given siren number' do
+      it 'creates a new dossier' do
+        all_dossiers_for_greffe = DossierEntreprise.where(code_greffe: data[:code_greffe])
+
+        expect { subject }.to change(all_dossiers_for_greffe, :count).by(1)
+      end
+
+      it 'returns a warning message' do
+        warning_msg = subject[:warning]
+
+        expect(warning_msg).to eq('Dossier (numero_gestion: 1A2B3C) not found and no dossier with the siren number 123456789 exist for greffe 1234. Creating new dossier.')
+      end
+
+      it { is_expected.to be_success }
+    end
+
+    # TODO What to do when multiple dossiers are found for a given siren
+    context 'when a dossier already exists for the give siren' do
+      before do
+        create(
+          :dossier_entreprise,
+          numero_gestion: '123ZZZ',
+          code_greffe: data[:code_greffe],
+          siren: data[:siren]
+        )
+      end
+
+      it 'updates this dossier' do
+        subject
+        updated_dossier = DossierEntreprise.find_by(code_greffe: data[:code_greffe], siren: data[:siren])
+
+        expect(updated_dossier).to have_attributes(
+          numero_gestion: '1A2B3C',
+          date_immatriculation: 'hier',
+          date_transfert: 'demain',
+          sans_activite: 'OUI',
+          date_debut_activite: 'A PAS ACTIVITE'
+        )
+      end
+
+      it 'returns a warning message' do
+        warning_msg = subject[:warning]
+
+        expect(warning_msg).to eq('Dossier (numero_gestion: 1A2B3C) not found for greffe 1234, but an existing dossier (numero_gestion: 123ZZZ) is found for siren 123456789 : updating this dossier instead.')
+      end
+
+      it { is_expected.to be_success }
+    end
   end
 
   context 'when the dossier is found' do
