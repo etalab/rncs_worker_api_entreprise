87f67233c155ad288ab23fce1da7fc204023ab7b
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-02-01 17:53:02 +0100

    Add a Supersede operation for Dossier Entreprise

--- app/concepts/dossier_entreprise/operation/supersede.rb
+++ app/concepts/dossier_entreprise/operation/supersede.rb
@@ -1,6 +1,25 @@
 class DossierEntreprise
   module Operation
     class Supersede < Trailblazer::Operation
+      step :find_existing_dossier, Output(:failure) => :save_new_dossier
+      step :destroy_old_dossier
+      step :save_new_dossier, id: :save_new_dossier
+
+
+      def find_existing_dossier(ctx, data:, **)
+        ctx[:old_dossier] = DossierEntreprise.find_by({
+          code_greffe: data[:code_greffe],
+          numero_gestion: data[:numero_gestion]
+        })
+      end
+
+      def destroy_old_dossier(ctx, old_dossier:, **)
+        old_dossier.destroy
+      end
+
+      def save_new_dossier(ctx, data:, **)
+        DossierEntreprise.create(data)
+      end
     end
   end
 end
---
+++ spec/concepts/dossier_entreprise/operation/supersede_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe DossierEntreprise::Operation::Supersede do
+  let(:data) do
+    {
+      code_greffe: '1234',
+      numero_gestion: '1A2B3C',
+      siren: '123456789',
+      date_immatriculation: 'today',
+      sans_activite: 'OUI',
+      date_debut_activite: 'A PAS ACTIVITE',
+    }
+  end
+
+  subject { described_class.call(data: data) }
+
+  context 'when the dossier exists in database' do
+    let(:old_dossier) do
+      create(:dossier_entreprise,
+             code_greffe: '1234',
+             numero_gestion: '1A2B3C',
+             siren: '123456789',
+             date_immatriculation: 'hier',
+             sans_activite: 'NON',
+            )
+    end
+
+    before { old_dossier }
+
+    it { is_expected.to be_success }
+
+    it 'destroy the old dossier' do
+      ghost = old_dossier
+      subject
+
+      expect{ ghost.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'saves the new one' do
+      subject
+      dossier = DossierEntreprise.find_by(code_greffe: '1234', numero_gestion: '1A2B3C')
+
+      expect(dossier).to be_persisted
+      expect(dossier.date_immatriculation).to eq('today')
+      expect(dossier.sans_activite).to eq('OUI')
+    end
+  end
+
+  context 'when the dossier is not save in database' do
+    it { is_expected.to be_success }
+
+    it 'saves the new one' do
+      subject
+      dossier = DossierEntreprise.find_by(code_greffe: '1234', numero_gestion: '1A2B3C')
+
+      expect(dossier).to be_persisted
+      expect(dossier.date_immatriculation).to eq('today')
+      expect(dossier.sans_activite).to eq('OUI')
+    end
+  end
+end
