52038f933f7a11531c88d38591fa32df14f5c182
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-02-04 13:06:03 +0100

    Refactor TC stock loading operation

---
+++ app/concepts/tribunal_commerce/stock/operation/import.rb
@@ -0,0 +1,75 @@
+module TribunalCommerce
+  module Stock
+    module Operation
+      class Import < Trailblazer::Operation
+        extend ClassDependencies
+        include TrailblazerHelper::DBIndexes
+
+        self[:stocks_folder] = ::File.join(Rails.configuration.rncs_sources['path'], 'tc/stock')
+        self[:logger] = Rails.logger
+
+        step :log_import_start
+        step :stock_exists?
+          fail :log_stock_not_found, fail_fast: true
+        step :save_new_stock
+        step :fetch_related_stock_units
+        step :drop_db_index
+        step ->(ctx, logger:, **) { logger.info('Creating jobs to import each greffe\'s unit synchronously...') }
+        step :import
+
+        def drop_db_index(ctx, **)
+          drop_queries.each { |query| ActiveRecord::Base.connection.execute(query) }
+        end
+
+        def stock_exists?(ctx, stock_args:, stocks_folder:, **)
+          stock_path = "#{stock_args[:year]}/#{stock_args[:month]}/#{stock_args[:day]}"
+          ctx[:stock_absolute_path] = ::File.join(stocks_folder, stock_path)
+          Dir.exists?(ctx[:stock_absolute_path])
+        end
+
+        def save_new_stock(ctx, stock_args:, stock_absolute_path:, **)
+          ctx[:new_stock] = StockTribunalCommerce.create(
+            year: stock_args[:year],
+            month: stock_args[:month],
+            day: stock_args[:day],
+            files_path: stock_absolute_path,
+          )
+        end
+
+        def fetch_related_stock_units(ctx, new_stock:, **)
+          Dir.each_child(new_stock.files_path) do |unit_name|
+            if match = unit_name.match(/\A(\d{4})_S(\d)_\d{8}\.zip\Z/)
+              code_greffe, unit_number = match.captures
+
+              new_stock.stock_units.create(
+                code_greffe: code_greffe,
+                number: unit_number,
+                file_path: ::File.join(new_stock.files_path, unit_name),
+                status: 'PENDING'
+              )
+            else
+              return false
+            end
+          end
+
+          return true
+        end
+
+        def import(ctx, new_stock:, **)
+          new_stock.stock_units.each do |unit|
+            ImportTcStockUnitJob.perform_later(unit.id)
+          end
+        end
+
+        def log_stock_not_found(ctx, logger:, stock_absolute_path:, **)
+          logger.error("Specified stock #{stock_absolute_path} is not found. Ensure the stock to import exists. Aborting...")
+        end
+
+        def log_import_start(ctx, logger:, stock_args:, **)
+          stock_name = "#{stock_args[:year]}/#{stock_args[:month]}/#{stock_args[:day]}"
+          logger.info("Starting import : looking for stock #{stock_name} in file system...")
+        end
+      end
+    end
+  end
+end
