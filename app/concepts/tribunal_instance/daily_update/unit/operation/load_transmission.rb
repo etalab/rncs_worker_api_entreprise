1561a830413f8e798657a1bcb32a55a10f7e490b
Alexis Leclerc <alexis@alxs.fr>
2019-03-05 14:10:28 +0200

    [TITMC] Daily update unit load

---
+++ app/concepts/tribunal_instance/daily_update/unit/operation/load_transmission.rb
@@ -0,0 +1,12 @@
+module TribunalInstance
+  module DailyUpdate
+    module Unit
+      module Operation
+        class LoadTransmission < Trailblazer::Operation
+
+          pass ->(ctx, **) {}
+        end
+      end
+    end
+  end
+end
