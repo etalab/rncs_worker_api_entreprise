3fc30dc85e07c535349130acc5b089bdfced8f2f
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-03-08 16:38:17 +0100

    Sync files with FTP every day at midnight
    First and naive implementation : should be done with a lib like Open3
    next.

--- app/jobs/sync_ftp_job.rb
+++ app/jobs/sync_ftp_job.rb
@@ -1,7 +1,17 @@
 class SyncFTPJob < ActiveJob::Base
   queue_as :auto_updates
 
+  attr_reader :ftp_login, :ftp_password
+
   def perform(*args)
-    Rails.logger.info 'Sync files from FTP...'
+    @ftp_login = Rails.application.credentials.ftp_login
+    @ftp_password = Rails.application.credentials.ftp_password
+
+    # Sync FTP files
+    `lftp -u '#{ftp_login}','#{ftp_password}' -p 21 opendata-rncs.inpi.fr -e 'mirror -c -P 4 --only-missing public/IMR_Donnees_Saisies ~/rncs_data/IMR_Donnees_Saisies; quit'`
+
+    # Ensure files permissions are correct
+    `find ~/rncs_data/IMR_Donnees_Saisies/tc -type d -exec chmod 755 {} +`
+    `find ~/rncs_data/IMR_Donnees_Saisies/tc -type f -exec chmod 644 {} +`
   end
 end
