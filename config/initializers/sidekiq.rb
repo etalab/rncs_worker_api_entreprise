4dff2aa7452a52492859c04e3c5bf353be81cb9e
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-03-28 21:19:45 +0100

    Set different cron time for daily jobs between environments to avoid conflicts
    Sidekiq Cron prevent the same job to be enqued multiple times at the same
    time and actually doesn't care about the active job's queue prefix we
    use to differentiate queues between instances environments. To fix this
    we just run the daily import in sandbox and production at different times.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@ Sidekiq.configure_server do |config|
   schedule_file = 'config/schedule.yml'
 
   if File.exists?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    loaded_conf = YAML.load_file(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash(loaded_conf[Rails.env])
   end
 end
