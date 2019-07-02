1cf0bc266207a56242faaac218708c9fc0cb787e
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-07-02 19:12:08 +0200

    Fix FTP sync action
    Only sync the current and the previous month every day now

--- app/jobs/sync_ftp_job.rb
+++ app/jobs/sync_ftp_job.rb
@@ -7,6 +7,7 @@ class SyncFTPJob < ActiveJob::Base
     if Rails.env.production?
       sync_files
       ensure_permissions_are_correct
+      clean_sync_folders
     end
   end
 
@@ -26,7 +27,14 @@ class SyncFTPJob < ActiveJob::Base
   end
 
   def lftp_command
-    "lftp -u '#{ftp_login}','#{ftp_password}' -p 21 opendata-rncs.inpi.fr -e 'mirror -c -P 4 public/IMR_Donnees_Saisies #{rncs_source_path}; quit'"
+    <<-ENDLFTP
+    lftp -u '#{ftp_login}','#{ftp_password}' -p 21 opendata-rncs.inpi.fr -e '
+      mirror -c -P 2 -F public/IMR_Donnees_Saisies/tc/flux/#{current_year}/#{current_month}/ -O #{rncs_source_path}/tc/flux/#{current_year};
+      mirror -c -P 2 -F public/IMR_Donnees_Saisies/tc/flux/#{previous_month_year}/#{previous_month}/ -O #{rncs_source_path}/tc/flux/#{previous_month_year};
+      mirror -c -P 2 -F public/IMR_Donnees_Saisies/tc/stock/#{current_year}/#{current_month}/ -O #{rncs_source_path}/tc/stock/#{current_year};
+      mirror -c -P 2 -F public/IMR_Donnees_Saisies/tc/stock/#{previous_month_year}/#{previous_month}/ -O #{rncs_source_path}/tc/stock/#{previous_month_year};
+      quit'
+    ENDLFTP
   end
 
   def rncs_source_path
@@ -39,5 +47,38 @@ class SyncFTPJob < ActiveJob::Base
 
   def ftp_password
     @ftp_password ||= Rails.application.credentials.ftp_password
+  end
+
+  def current_year
+    @current_year ||= Time.now.strftime("%Y")
+  end
+
+  def current_month
+    @current_month ||= Time.now.strftime("%m")
+  end
+
+  def previous_month_year
+    @previous_month_year ||= (Time.now.beginning_of_month - 1.day).strftime("%Y")
+  end
+
+  def previous_month
+    @previous_month ||= (Time.now.beginning_of_month - 1.day).strftime("%m")
+  end
+
+  def clean_sync_folders
+    sync_folders = [
+      "#{rncs_source_path}/tc/flux/#{current_year}/#{current_month}",
+      "#{rncs_source_path}/tc/flux/#{current_year}",
+      "#{rncs_source_path}/tc/flux/#{previous_month_year}/#{previous_month}",
+      "#{rncs_source_path}/tc/flux/#{previous_month_year}",
+      "#{rncs_source_path}/tc/stock/#{current_year}/#{current_month}",
+      "#{rncs_source_path}/tc/stock/#{current_year}",
+      "#{rncs_source_path}/tc/stock/#{previous_month_year}/#{previous_month}",
+      "#{rncs_source_path}/tc/stock/#{previous_month_year}",
+    ]
+
+    sync_folders.each do |dir|
+      FileUtils.rm_rf(dir) if Dir.exists?(dir) && Dir.empty?(dir)
+    end
   end
 end
--- spec/jobs/sync_ftp_job_spec.rb
+++ spec/jobs/sync_ftp_job_spec.rb
@@ -3,24 +3,112 @@ require 'rails_helper'
 describe SyncFTPJob do
   subject { described_class.perform_now }
 
+  before do
+    # Freeze time in January so we directly test the most edged case :
+    # new year transition with sync of the January month of the current
+    # year and the sync of the December month of the previous year
+    Timecop.freeze(2019, 1, 10)
+  end
+
+  after { Timecop.return }
+
   context 'when in production environment' do
+    let(:rncs_source_path) { Rails.configuration.rncs_sources['path'] }
+
     before { allow(Rails.env).to receive(:production?).and_return(true) }
 
-    describe 'when all command are successful' do
+    describe 'sync job behaviour' do
       before do
         allow_lftp_success
         allow_chmod_success
       end
 
-      it 'calls the commands lines ordered' do
+      it 'syncs TC daily updates for the current month' do
+        current_month_sync_command = "mirror -c -P 2 -F public/IMR_Donnees_Saisies/tc/flux/2019/01/ -O #{rncs_source_path}/tc/flux/2019;"
+        expect(Open3).to receive(:capture3)
+          .with(a_string_including(current_month_sync_command))
+
+        subject
+      end
+
+      it 'syncs TC daily updates for the previous month' do
+        previous_month_sync_command = "mirror -c -P 2 -F public/IMR_Donnees_Saisies/tc/flux/2018/12/ -O #{rncs_source_path}/tc/flux/2018;"
+        expect(Open3).to receive(:capture3)
+          .with(a_string_including(previous_month_sync_command))
+
+        subject
+      end
+
+      it 'syncs TC partial stocks for the current month' do
+        current_month_sync_command = "mirror -c -P 2 -F public/IMR_Donnees_Saisies/tc/stock/2019/01/ -O #{rncs_source_path}/tc/stock/2019;"
+        expect(Open3).to receive(:capture3)
+          .with(a_string_including(current_month_sync_command))
+
+        subject
+      end
+
+      it 'syncs TC partial stocks for the previous month' do
+        previous_month_sync_command = "mirror -c -P 2 -F public/IMR_Donnees_Saisies/tc/stock/2018/12/ -O #{rncs_source_path}/tc/stock/2018;"
+        expect(Open3).to receive(:capture3)
+          .with(a_string_including(previous_month_sync_command))
+
+        subject
+      end
+
+      it 'changes file permissions of the files and folders to deploy after sync' do
+        change_dirs_permissions_cmd = "find #{rncs_source_path} -type d -exec chmod 755 {} +"
+        change_files_permissions_cmd = "find #{rncs_source_path} -type f -exec chmod 644 {} +"
         expect(Open3).to receive(:capture3).with(/lftp/).ordered
-        expect(Open3).to receive(:capture3).with(/chmod/).twice.ordered
+        expect(Open3).to receive(:capture3).with(change_dirs_permissions_cmd).ordered
+        expect(Open3).to receive(:capture3).with(change_files_permissions_cmd).ordered
+
         subject
       end
 
       it 'does not log any error' do
         expect(Rails.logger).not_to receive(:error)
         subject
+      end
+    end
+
+    # When specifying folders to sync as we do with lftp, the executed command
+    # will create the targetted folders on the local file system even if they
+    # are empty (because inexistant on the FTP source server). This will happen
+    # a lot for partial stock folders since they can be absent for several
+    # months. We purge such empty folders after the sync since we don't want
+    # the code for import to fail because it founds empty folders on the disk.
+    describe 'sync folders post traitment' do
+      before do
+        # Mock lftp system call and manually create targetted folders
+        expect(Open3).to receive(:capture3)
+          .and_wrap_original do |original_method, *args|
+          FileUtils.mkdir_p("#{rncs_source_path}/tc/stock/2018/12")
+          FileUtils.touch("#{rncs_source_path}/tc/stock/2018/12/a_file.txt")
+          FileUtils.mkdir_p("#{rncs_source_path}/tc/flux/2019/01")
+          ['', '', status_success]
+        end
+
+        allow_chmod_success
+      end
+
+      after { FileUtils.rm_rf("#{rncs_source_path}/tc/stock/2018/12") }
+
+      it 'deletes empty month folder' do
+        subject
+
+        expect(Dir.exists?("#{rncs_source_path}/tc/flux/2019/01")).to eq(false)
+      end
+
+      it 'deletes empty year folder' do
+        subject
+
+        expect(Dir.exists?("#{rncs_source_path}/tc/flux/2019")).to eq(false)
+      end
+
+      it 'keeps non empty folder' do
+        subject
+
+        expect(Dir.exists?("#{rncs_source_path}/tc/stock/2018/12")).to eq(true)
       end
     end
 
@@ -62,11 +150,11 @@ describe SyncFTPJob do
   private
 
   def allow_chmod_success
-      allow(Open3).to receive(:capture3).with(/chmod/).and_call_original
+    allow(Open3).to receive(:capture3).with(/chmod/).and_call_original
   end
 
   def allow_lftp_success
-      allow(Open3).to receive(:capture3).with(/lftp.+/).and_return(['', '', status_success])
+    allow(Open3).to receive(:capture3).with(/lftp.+/).and_return(['', '', status_success])
   end
 
   def status_success
