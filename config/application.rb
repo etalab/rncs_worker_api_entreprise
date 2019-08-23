cec682f0f5063434b10fd3cb39addae92edd133a
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-08-23 15:10:17 +0200

    Use Rails 6.0 default configuration

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module RncsWorkerApiEntreprise
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults '6.0'
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
