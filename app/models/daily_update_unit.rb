89bd7af8d69f780ffe74a819b39ded34fe54e34f
Alexandre De Pablo <alexandre.depablo@gmail.com>
2019-11-28 14:31:59 +0100

    Delete dead code

--- app/models/daily_update_unit.rb
+++ app/models/daily_update_unit.rb
@@ -6,14 +6,4 @@ class DailyUpdateUnit < ApplicationRecord
     file_path = Rails.root.join('log', filename).to_s
     Logger.new(file_path)
   end
-
-  private
-
-  def formated_daily_update_date
-    [daily_update.year, daily_update.month, daily_update.day].join('')
-  end
-
-  def formated_current_time
-    Time.now.strftime('%Y_%m_%d')
-  end
 end
--- app/models/stock_unit.rb
+++ app/models/stock_unit.rb
@@ -18,8 +18,4 @@ class StockUnit < ApplicationRecord
   def stock_date
     [stock.year, stock.month, stock.day].join('')
   end
-
-  def formatted_current_datetime
-    Time.now.strftime('%Y_%m_%d__%H_%M_%S')
-  end
 end
