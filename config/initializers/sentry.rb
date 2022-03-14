fcfdf463c20a0b55a23c0050df3e310a800c0db5
Coder1221 <21100328@lums.edu.pk>
2022-03-14 16:55:44 +0500

    Commented out sidekiq

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
-Raven.configure do |config|
-  config.dsn = Rails.application.credentials.url_sentry
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = %w[production]
-end
+# Raven.configure do |config|
+#   config.dsn = Rails.application.credentials.url_sentry
+#   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+#   config.environments = %w[production]
+# end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,17 +3,29 @@ def load_sidekiq_cron_jobs
 
   if File.exists?(schedule_file)
     loaded_conf = YAML.load_file(schedule_file)
+    p '=================LOADED CRON JOBS============='
     Sidekiq::Cron::Job.load_from_hash(loaded_conf[Rails.env])
   end
 end
 
+sidekiq_config = { url: ENV['REDIS_URL'] }
+
+p '============================================================'
+# sidekiq_config = { url: 'redis:6379/0' }
+# sidekiq_config = { url: 'redis://redis:6379'}
+# sidekiq_config = { url: 'localhost://redis:6379'}
+p sidekiq_config
+p  Rails.env
+p '============================================================'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.configuration.redis_database }
-
+  # config.redis = { url: Rails.configuration.redis_database }
+  config.redis = sidekiq_config
   load_sidekiq_cron_jobs
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.configuration.redis_database }
+  # config.redis = { url: Rails.configuration.redis_database }
+  config.redis = sidekiq_config
+  load_sidekiq_cron_jobs
 end
